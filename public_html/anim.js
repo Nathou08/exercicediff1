a619e698120df55515deff841100e3c11c878f4b
UTILISATEUR <UTILISATEUR@192.168.1.167>
2017-07-26 09:40:23 +0200

    Diff 1 fini !

--- public_html/anim.js
+++ public_html/anim.js
@@ -1,8 +1,19 @@
 function converter(){
-    var x = 20;
-    var euro = 1.16998 ;
-    var result = x*euro;
-    console.log(result);
+        var x = parseInt(document.getElementById("saisie").value);
+        var select = document.getElementById("selection").value;
+        var dollar = document.getElementById("dollar").value;
+        var livre = document.getElementById("livre").value;
+        var yen = document.getElementById("yen").value;
+        
+        if(select == dollar){
+            document.getElementById("resultat").value = x*1.16998;
+        }
+        else if(select == livre){
+            document.getElementById("resultat").value = x*0.89370;
+        }
+        else if(select == yen){
+            document.getElementById("resultat").value = x*130.13900;
+        }
 }
 
 
